f82f7ed6c25a1f76456239be8e52765177c924fe
polux0 <afidelo@protonmail.com>
2023-12-07 17:13:21 +0100

    fix: moved `BaalSummoner` & `IBaalToken` into separate contract;

---
+++ script/Baal.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console2} from "forge-std/Script.sol";
+import '../src/Baal.sol';
+
+contract BaalScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        
+        SharesERC20 sharesERC20 = new SharesERC20();
+        vm.stopBroadcast();
+    }
+    // function setUp() public {
+
+    // }
+}
